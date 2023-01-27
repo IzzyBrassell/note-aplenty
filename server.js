b0bd09778912b78af994be876405e5e82ff4b877
Israel Brassell <israel.brassell@gmail.com>
2023-01-26 21:04:22 -0500

    "connected notes.html to server.js"

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const path = require('path');
+const fs = require('fs')
 const app = express()
 
 const PORT = process.env.PORT || 5001
@@ -7,11 +9,13 @@ app.use(express.static('public'))
 app.use(express.static('db'))
 
 app.get('/', (req, res) => {
-    res.sendFile('index.html')
+    res.send('index.html')
 })
 
-// app.get('/notes', (req, res) => {
-//     res.sendFile(`notes.html`)
-// })  
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname,'public','notes.html'))
+});
+
+
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
